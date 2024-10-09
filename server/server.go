77bc3164f0c2f8aec04b66afe9ce315eacebeab0
RonexLemon <ronexondimu@gmail.com>
2024-10-09 06:03:40 +0300

    Add

--- server/server.go
+++ server/server.go
@@ -2,26 +2,23 @@ package server
 
 import "errors"
 
-
-
-type Args struct{
-	A,B int
+type Args struct {
+	A, B int
 }
 
-type Quotient struct{
+type Quotient struct {
 	Quo, Rem int
 }
 
-type Arith  int
+type Arith int
 
-
-func(a *Arith) Multiply(ar *Args, result *int)error{
-	*result =ar.A *ar.B
+func (a *Arith) Multiply(ar *Args, result *int) error {
+	*result = ar.A * ar.B
 	return nil
 }
 
-func(a *Arith) Divide(ar *Args, quo *Quotient)error{
-	if ar.B == 0{
+func (a *Arith) Divide(ar *Args, quo *Quotient) error {
+	if ar.B == 0 {
 		return errors.New("Divide By Zero")
 	}
 	quo.Quo = ar.A / ar.B
@@ -29,4 +26,10 @@ func(a *Arith) Divide(ar *Args, quo *Quotient)error{
 
 	return nil
 
+}
+
+func (a *Arith) Add(ar *Args, result *int) error {
+
+	*result = ar.A + ar.B
+	return nil
 }
