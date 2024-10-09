29930f957b8a1759a6aff7840c0b673397b44553
RonexLemon <ronexondimu@gmail.com>
2024-10-09 06:11:50 +0300

    serve route

--- server/server.go
+++ server/server.go
@@ -1,6 +1,12 @@
 package server
 
-import "errors"
+import (
+	"errors"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+)
 
 type Args struct {
 	A, B int
@@ -32,4 +38,19 @@ func (a *Arith) Add(ar *Args, result *int) error {
 
 	*result = ar.A + ar.B
 	return nil
+}
+
+
+func Server(){
+	arith := new(Arith)
+	rpc.Register(arith)
+	rpc.HandleHTTP()
+	l,err := net.Listen( "tcp", ":1234")
+	if err != nil {
+		log.Fatal("listen error:", err)
+	}
+
+	go http.Serve(l,nil)
+
+
 }
