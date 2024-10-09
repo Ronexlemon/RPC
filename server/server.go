3521475d904f494a7f7a6854b1a8bf6c88210630
RonexLemon <ronexondimu@gmail.com>
2024-10-09 06:01:50 +0300

    divide

--- server/server.go
+++ server/server.go
@@ -1,5 +1,7 @@
 package server
 
+import "errors"
+
 
 
 type Args struct{
@@ -18,4 +20,13 @@ func(a *Arith) Multiply(ar *Args, result *int)error{
 	return nil
 }
 
-func(a *Arith) Divide
+func(a *Arith) Divide(ar *Args, quo *Quotient)error{
+	if ar.B == 0{
+		return errors.New("Divide By Zero")
+	}
+	quo.Quo = ar.A / ar.B
+	quo.Rem = ar.A % ar.B
+
+	return nil
+
+}
